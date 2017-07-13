76a627c561f249e3cdcad69fe5b8371dd028ac4d
bang <sqibang@gmail.com>
2017-07-13 23:15:18 +0700

    chore: format

--- ios/MerryToast.h
+++ ios/MerryToast.h
@@ -5,15 +5,15 @@
 #import <React/RCTBridgeModule.h>
 #endif
 
-#import <UIKit/UIKit.h>
 #import <Toast/UIView+Toast.h>
+#import <UIKit/UIKit.h>
 
-NSInteger const LRDRCTSimpleToastBottomOffset = 40;
-double const LRDRCTSimpleToastShortDuration = 3.0;
-double const LRDRCTSimpleToastLongDuration = 5.0;
-NSInteger const LRDRCTSimpleToastGravityBottom = 1;
-NSInteger const LRDRCTSimpleToastGravityCenter = 2;
-NSInteger const LRDRCTSimpleToastGravityTop = 3;
+NSInteger const MerryToastBottomOffset = 40;
+double const MerryToastShortDuration = 3.0;
+double const MerryToastLongDuration = 5.0;
+NSInteger const MerryToastGravityBottom = 1;
+NSInteger const MerryToastGravityCenter = 2;
+NSInteger const MerryToastGravityTop = 3;
 
 @interface MerryToast : NSObject <RCTBridgeModule>
 
--- ios/MerryToast.m
+++ ios/MerryToast.m
@@ -1,106 +1,119 @@
 
 #import "MerryToast.h"
 
-@implementation MerryToast{
-    CGFloat _keyOffset;
+@implementation MerryToast {
+  CGFloat _keyOffset;
 }
 RCT_EXPORT_MODULE()
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
+- (dispatch_queue_t)methodQueue {
+  return dispatch_get_main_queue();
 }
 - (instancetype)init {
-    if (self = [super init]) {
-        _keyOffset = 0;
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(keyboardWasShown:)
-                                                     name:UIKeyboardDidShowNotification
-                                                   object:nil];
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(keyboardWillHiden:)
-                                                     name:UIKeyboardWillHideNotification
-                                                   object:nil];
-    }
-    return self;
+  if (self = [super init]) {
+    _keyOffset = 0;
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self
+           selector:@selector(keyboardWasShown:)
+               name:UIKeyboardDidShowNotification
+             object:nil];
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self
+           selector:@selector(keyboardWillHiden:)
+               name:UIKeyboardWillHideNotification
+             object:nil];
+  }
+  return self;
 }
 
 - (void)keyboardWasShown:(NSNotification *)notification {
-    
-    CGSize keyboardSize = [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
-    
-    int height = MIN(keyboardSize.height,keyboardSize.width);
-    int width = MAX(keyboardSize.height,keyboardSize.width);
-    
-    _keyOffset = height;
+
+  CGSize keyboardSize =
+      [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey]
+          CGRectValue]
+          .size;
+
+  int height = MIN(keyboardSize.height, keyboardSize.width);
+  int width = MAX(keyboardSize.height, keyboardSize.width);
+
+  _keyOffset = height;
 }
 
 - (void)keyboardWillHiden:(NSNotification *)notification {
-    _keyOffset = 0;
+  _keyOffset = 0;
 }
 
 - (NSDictionary *)constantsToExport {
-    return @{
-             @"SHORT": [NSNumber numberWithDouble:LRDRCTSimpleToastShortDuration],
-             @"LONG": [NSNumber numberWithDouble:LRDRCTSimpleToastLongDuration],
-             @"BOTTOM": [NSNumber numberWithInteger:LRDRCTSimpleToastGravityBottom],
-             @"CENTER": [NSNumber numberWithInteger:LRDRCTSimpleToastGravityCenter],
-             @"TOP": [NSNumber numberWithInteger:LRDRCTSimpleToastGravityTop]
-             };
+  return @{
+    @"SHORT" : [NSNumber numberWithDouble:MerryToastShortDuration],
+    @"LONG" : [NSNumber numberWithDouble:MerryToastLongDuration],
+    @"BOTTOM" : [NSNumber numberWithInteger:MerryToastGravityBottom],
+    @"CENTER" : [NSNumber numberWithInteger:MerryToastGravityCenter],
+    @"TOP" : [NSNumber numberWithInteger:MerryToastGravityTop]
+  };
 }
 //
-//RCT_EXPORT_METHOD(show:(NSString *)msg duration:(double)duration {
-//    [self _show:msg duration:duration gravity:LRDRCTSimpleToastGravityBottom];
+// RCT_EXPORT_METHOD(show:(NSString *)msg duration:(double)duration {
+//    [self _show:msg duration:duration gravity:MerryToastGravityBottom];
 //});
 //
-//RCT_EXPORT_METHOD(showWithGravity:(NSString *)msg duration:(double)duration gravity:(nonnull NSNumber *)gravity{
+// RCT_EXPORT_METHOD(showWithGravity:(NSString *)msg duration:(double)duration
+// gravity:(nonnull NSNumber *)gravity{
 //    [self _show:msg duration:duration gravity:gravity.intValue];
 //});
-//- (UIViewController *)visibleViewController:(UIViewController *)rootViewController
+//- (UIViewController *)visibleViewController:(UIViewController
+//*)rootViewController
 //{
 //    if (rootViewController.presentedViewController == nil)
 //    {
 //        return rootViewController;
 //    }
-//    if ([rootViewController.presentedViewController isKindOfClass:[UINavigationController class]])
+//    if ([rootViewController.presentedViewController
+//    isKindOfClass:[UINavigationController class]])
 //    {
-//        UINavigationController *navigationController = (UINavigationController *)rootViewController.presentedViewController;
-//        UIViewController *lastViewController = [[navigationController viewControllers] lastObject];
-//        
+//        UINavigationController *navigationController = (UINavigationController
+//        *)rootViewController.presentedViewController; UIViewController
+//        *lastViewController = [[navigationController viewControllers]
+//        lastObject];
+//
 //        return [self visibleViewController:lastViewController];
 //    }
-//    if ([rootViewController.presentedViewController isKindOfClass:[UITabBarController class]])
+//    if ([rootViewController.presentedViewController
+//    isKindOfClass:[UITabBarController class]])
 //    {
-//        UITabBarController *tabBarController = (UITabBarController *)rootViewController.presentedViewController;
-//        UIViewController *selectedViewController = tabBarController.selectedViewController;
-//        
+//        UITabBarController *tabBarController = (UITabBarController
+//        *)rootViewController.presentedViewController; UIViewController
+//        *selectedViewController = tabBarController.selectedViewController;
+//
 //        return [self visibleViewController:selectedViewController];
 //    }
-//    
-//    UIViewController *presentedViewController = (UIViewController *)rootViewController.presentedViewController;
-//    
+//
+//    UIViewController *presentedViewController = (UIViewController
+//    *)rootViewController.presentedViewController;
+//
 //    return [self visibleViewController:presentedViewController];
 //}
 //
-//- (void)_show:(NSString *)msg duration:(NSTimeInterval)duration gravity:(NSInteger)gravity {
+//- (void)_show:(NSString *)msg duration:(NSTimeInterval)duration
+//gravity:(NSInteger)gravity {
 //    dispatch_async(dispatch_get_main_queue(), ^{
-//        //UIView *root = [[[[[UIApplication sharedApplication] delegate] window] rootViewController] view];
-//        UIViewController *ctrl = [self visibleViewController:[UIApplication sharedApplication].keyWindow.rootViewController];
-//        UIView *root = [ctrl view];
-//        CGRect bound = root.bounds;
-//        bound.size.height -= _keyOffset;
-//        if (bound.size.height > LRDRCTSimpleToastBottomOffset*2) {
-//            bound.origin.y += LRDRCTSimpleToastBottomOffset;
-//            bound.size.height -= LRDRCTSimpleToastBottomOffset*2;
+//        //UIView *root = [[[[[UIApplication sharedApplication] delegate]
+//        window] rootViewController] view]; UIViewController *ctrl = [self
+//        visibleViewController:[UIApplication
+//        sharedApplication].keyWindow.rootViewController]; UIView *root = [ctrl
+//        view]; CGRect bound = root.bounds; bound.size.height -= _keyOffset; if
+//        (bound.size.height > MerryToastBottomOffset*2) {
+//            bound.origin.y += MerryToastBottomOffset;
+//            bound.size.height -= MerryToastBottomOffset*2;
 //        }
 //        UIView *view = [[UIView alloc] initWithFrame:bound];
 //        view.userInteractionEnabled = NO;
 //        [root addSubview:view];
 //        UIView __weak *blockView = view;
 //        id position;
-//        if (gravity == LRDRCTSimpleToastGravityTop) {
+//        if (gravity == MerryToastGravityTop) {
 //            position = CSToastPositionTop;
-//        } else if (gravity == LRDRCTSimpleToastGravityCenter) {
+//        } else if (gravity == MerryToastGravityCenter) {
 //            position = CSToastPositionCenter;
 //        } else {
 //            position = CSToastPositionBottom;
@@ -117,4 +130,3 @@ RCT_EXPORT_MODULE()
 //    });
 //}
 @end
-  
